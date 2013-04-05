22cbc6f56f3136942cd95492dc37db4e55733d81
Dmitry Bobrov <me@dbobrov.com>
2013-04-05 23:18:20 +0400

    Revert "std=c++11; removed stdafx.h"
    This reverts commit a16ff857823e3e9bead0b198fb193ec2f7cd60d2.

---
+++ src/stdafx.h
@@ -0,0 +1,14 @@
+#include <vector>
+#include <stack>
+
+#include <QtOpenGL>
+#include <QColor>
+
+#include <boost/cstdint.hpp>
+#include <boost/range/algorithm/copy.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/smart_ptr.hpp>
+
+#include <boost/optional.hpp>
+using boost::optional;
