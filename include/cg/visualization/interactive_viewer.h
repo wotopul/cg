0517f3279b332a47ac9e985554076d998291fc69
lisyarus <lisyarus@gmail.com>
2013-04-13 21:11:47 +0400

    Interactive viewer added

---
+++ include/cg/visualization/interactive_viewer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "viewer_adapter.h"
+#include "interactive/interactive.h"
+
+#include <boost/optional.hpp>
+
+namespace cg {
+namespace visualization {
+
+   struct interactive_viewer : viewer_adapter
+   {
+      interactive_viewer ( );
+
+      void draw(cg::visualization::drawer_type & drawer) const override;
+      
+      bool on_press   (const point_2f & pos) override;
+      bool on_release (const point_2f & pos) override;
+      bool on_move    (const point_2f & pos) override;
+
+   protected:
+
+      std::vector<cg::visualization::interactive_type *> interactive_;
+      float choose_distance_;
+
+   private:     
+
+      boost::optional<cg::visualization::interactive_type *> chosen_;
+      bool pressed_;
+      
+   };
+
+}}
