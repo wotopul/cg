a7288daeb89b7434ff1c378d51627b30b51e4673
wotopul <wotopul@gmail.com>
2013-11-23 18:17:53 +0400

    visualisation: ok, simple structures for cell added

--- examples/triangulation/delaunay.cpp
+++ examples/triangulation/delaunay.cpp
@@ -1,8 +1,11 @@
 #include <QColor>
 #include <QApplication>
 
+#include <algorithm>
 #include <boost/optional.hpp>
 
+#include <cg/primitives/point.h>
+#include <cg/primitives/triangle.h>
 #include <cg/triangulation/delaunay_triangulation.h>
 #include <cg/visualization/viewer_adapter.h>
 #include <cg/visualization/draw_util.h>
@@ -23,30 +26,37 @@ struct delaunay_viewer : cg::visualization::viewer_adapter
 
    void draw(cg::visualization::drawer_type & drawer) const
    {
-       for (size_t i = 0; i + 2 < pts.size(); i += 3)
-       {
-           drawer.set_color(Qt::white);
-           cg::triangle_2 tr(pts[i], pts[i + 1], pts[i + 2]);
-           cg::visualization::draw(drawer, tr);
-           drawer.set_color(Qt::green);
-           cg::visualization::draw(drawer, tr.center(), tr.radius());
-       }
+       std::for_each(answer.begin(), answer.end(),
+           [&](cg::triangle_2 tr)
+           {
+               drawer.set_color(Qt::white);
+               cg::visualization::draw(drawer, tr);
+               drawer.set_color(Qt::green);
+               cg::visualization::draw(drawer, tr.center(), tr.radius());
+           });
    }
 
    bool on_release(const point_2f & p)
    {
       pts.push_back(p);
-      return pts.size() > 2;
+      // triangulation.add(p);
+      //return pts.size() > 2;
+      answer.clear();
+      triangulation.triangulate(std::back_inserter< std::vector<cg::triangle_2> >(answer));
+      return true;
    }
 
    bool on_double_click(const point_2f & p)
    {
        pts.clear();
+       // triangulation.clear();
        return true;
    }
 
 private:
    std::vector<point_2> pts;
+   cg::delaunay_triangulation<double> triangulation;
+   std::vector<cg::triangle_2> answer;
 };
 
 int main(int argc, char ** argv)
--- include/cg/triangulation/delaunay_triangulation.h
+++ include/cg/triangulation/delaunay_triangulation.h
@@ -1,16 +1,56 @@
 #pragma once
 
+#include <boost/shared_ptr.hpp>
+
+#include <cg/primitives/point.h>
+#include <cg/primitives/triangle.h>
+
 namespace cg
 {
+using boost::shared_ptr;
+
+template <class Scalar>
+struct edge;
+
+template <class Scalar>
+struct vertex
+{
+    vertex() {}
+
+    shared_ptr< edge<Scalar> > out;
+};
+
+template <class Scalar>
+struct edge
+{
+    edge() {}
+
+    shared_ptr< vertex<Scalar> > begin;
+    shared_ptr< edge<Scalar> > next;
+    shared_ptr< edge<Scalar> > twin;
+};
+
+template <class Scalar>
+struct face
+{
+    face() {}
+
+    shared_ptr< edge<Scalar> > side;
+};
 
 template <class Scalar>
 class delaunay_triangulation
 {
-
 public:
     delaunay_triangulation() {}
-    void add(point_2t<Scalar> p) {}
+    void add(point_2t<Scalar> p);
 
+    template <class OutIter>
+    OutIter triangulate(OutIter out)
+    {
+        *out++ = cg::triangle_2(point_2(1, 1), point_2(60, 60), point_2(60, 1));
+        return out;
+    }
 };
 
 }
